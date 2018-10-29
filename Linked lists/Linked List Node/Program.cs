8d2b30e6223430c7602c2e5a53f15749117ae9d3
todai88 <joabaj88@gmail.com>
2018-10-29 20:52:27 +0000

    Initial commit

---
+++ Linked lists/Linked List Node/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Linked_List_Node
+{
+    class Program
+    {
+        public class LinkedListNode<T>
+        {
+            public LinkedListNode(T value)
+            {
+                Value = value;
+            }
+
+            public T Value {get; set;}
+
+            public LinkedListNode<T> Next {get; set;}
+        }
+
+        public class LinkedList<T> : 
+            System.Collections.Generic.ICollection<T>
+        {
+            public LinkedListNode<T> Head
+            {
+                get;
+                private set;
+            }
+
+            public LinkedListNode<T> Tail
+            {
+                get;
+                private set;
+            }
+
+            int ICollection<T>.Count => throw new NotImplementedException();
+
+            bool ICollection<T>.IsReadOnly => throw new NotImplementedException();
+#region Add Methods
+            public void AddFirst(T value)
+            {
+                AddFirst(new LinkedListNode<T>(value));
+            }
+
+            public void AddFirst(LinkedListNode<T> node)
+            {
+                // Keep current head in memory
+                var tmp = Head;
+
+                // Set the new value to the head
+                Head = node;
+
+                // Insert the rest behind the new node
+                Head.Next = tmp;
+
+                Count++;
+
+                if (Count == 0)
+                {
+                    Head = Tail;
+                }
+            }
+
+            public void AddLast(T value)
+            {
+                AddLast(new LinkedListNode<T>(value));
+            }
+
+            public void AddLast(LinkedListNode<T> node)
+            {
+                if (Count == 0)
+                {
+                    Head = node;
+                }
+                else 
+                {
+                    Tail.Next = node;
+                }
+
+                Tail = node;
+
+                Count++;
+            }
+#endregion Add
+#region Remove Methods
+        public void RemoveFirst()
+        {
+            if (Count != 0)
+            {
+                Head = Head.Next;
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+            }
+        }
+
+        public void RemoveLast()
+        {
+            // Nothing to remove 
+            if (Count != 0)
+            {
+                // If there is only one node, it's removed
+                if (Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else 
+                {
+                    // Find the last node and remove it by iterating through nexts.
+                    var current = Head;
+                    while(current.Next != Tail)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = null;
+                    Tail = current;
+                }
+                Count--;
+            }
+        }
+#endregion Remove Methods
+#region ICollection 
+            public int Count 
+            {
+                get;
+                private set;
+            }
+            void ICollection<T>.Add(T item)
+            {
+                AddFirst(item);
+            }
+
+            void ICollection<T>.Clear()
+            {
+                Head = null;
+                Tail = null;
+                Count = 0;
+            }
+
+            bool ICollection<T>.Contains(T item)
+            {
+                var current = Head;
+                while (current != null)
+                {
+                    if (current.Value.Equals(item))
+                    {
+                        return true;
+                    }
+                    current = current.Next;
+                }
+                return false;
+            }
+
+            void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+            {
+                var current = Head;
+                while(current != null)
+                {
+                    array[arrayIndex++] = current.Value;
+                    current = current.Next;
+                }
+            }
+            public bool IsReadOnly
+            {
+                get 
+                {
+                    return false;
+                }
+            }
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                LinkedListNode<T> current = Head;
+                while (current != null)
+                {
+                    yield return current.Value;
+                    current = current.Next;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((System.Collections.Generic.IEnumerable<T>)this).GetEnumerator();
+            }
+
+            bool ICollection<T>.Remove(T item)
+            {
+                LinkedListNode<T> previous = null;
+                var current = Head;
+
+                while(current != null)
+                {
+                    // Found it
+                    if (current.Value.Equals(item))
+                    {
+                        if (previous != null)
+                        {
+                            // Before Remove(6): 3 -> 7 -> 6 -> null
+                            // After  Remove(6): 3 -> 7 ------> null
+                            previous.Next = current.Next;
+
+                            if (current.Next == null)
+                            {
+                                Tail = previous;
+                            }
+
+                            Count--;
+                        }
+                        else
+                        {
+                            RemoveFirst();
+                        }
+                        return true;
+                    }
+
+                previous = current;
+                current = current.Next;
+                }
+                return false;
+            }
+        }
+#endregion ICollection
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Linked lists/Primetive Linked List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Linked_lists
+{
+    class Program
+    {
+        public class Node 
+        {
+            public int Value {get; set;}
+            public Node next {get; set;}
+        }
+        static void Main(string[] args)
+        {
+           Node first = new Node() { Value = 3 };
+           Node second = new Node() { Value = 10 };
+           first.next = second;
+
+           Console.WriteLine(first.next.Value);
+        }
+    }
+}
