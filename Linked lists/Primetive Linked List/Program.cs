c133bd69be89c5509b14371c2d8740906e1c3a89
Joakim Kakaei <joabaj88@gmail.com>
2018-10-30 08:40:36 +0000

    Changed the gitignore file to not ignore .csproj files, as the program becomes unable to compile

--- Linked lists/Primetive Linked List/Program.cs
+++ Linked lists/Primetive Linked List/Program.cs
@@ -11,11 +11,11 @@ namespace Linked_lists
         }
         static void Main(string[] args)
         {
-           Node first = new Node() { Value = 3 };
-           Node second = new Node() { Value = 10 };
-           first.next = second;
+            Node first = new Node() { Value = 3 };
+            Node second = new Node() { Value = 10 };
+            first.next = second;
 
-           Console.WriteLine(first.next.Value);
+            Console.WriteLine(first.next.Value);
         }
     }
 }
